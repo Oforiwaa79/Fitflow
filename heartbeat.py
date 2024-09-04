b176055650d4a1cb084b33ff2ba65d8f51d18400
oforiwaa <oforiwaaanarfi@gmail.com>
2024-09-04 02:08:51 -0900

    updated max30102.py

--- heartbeat.py
+++ heartbeat.py
@@ -1,14 +1,34 @@
+import time
 from max30102 import max30102
 from max30102 import hrcalc
 
+# Initialize the MAX30102 sensor
 m = max30102.MAX30102()
 
-red, ir = m.read_sequential() # get LEDs readings
+# Record the start time
+start_time = time.time()
 
-print(red, ir)
+try:
+    while True:
+    # Read the LED values
+        red, ir = m.read_sequential()
 
-print('\n\n')
+    # Print the readings
+    # print(red, ir)
+    #print('\n\n')
 
-print(f'my heart rate -> {hrcalc.calc_hr_and_spo2(ir[:100], red[:100])}')
+    # Calculate and print heart rate and SpO2
+        print(f'my heart rate -> {hrcalc.calc_hr_and_spo2(ir[:100], red[:100])}')
 
-m.shutdown()
+
+
+except KeyboardInterrupt:
+
+    # Check if 30 seconds have passed
+    # current_time = time.time()
+    # print(current_time)
+    # if current_time - start_time > 30:
+    #     break
+
+# Shutdown the sensor
+    m.shutdown()
